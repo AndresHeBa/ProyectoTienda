6f314295cfe731d8eee0ab13c3b48320dd9cc00d
AndresHeBa <124836762+AndresHeBa@users.noreply.github.com>
2023-11-30 16:21:27 -0600

    header

--- header.php
+++ header.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <link rel="stylesheet" href="css/header_style.css">
 </head>
+
 <body>
-    <header>
-        <div>
-            <img src="img/logo_transparent.png" alt="">
+<header>
+        <div class="row menu">
+            <div class="col-6 head logo_head">
+                <img id="logo" src="img/logo_transparent.png" alt="" height="60px">
+            </div>
+            <div class="col head">
+                <ul id="enlaces">
+                    <li><a href="principal.php">Inicio</a></li>
+                    <li><a href="">Sobre Nosotros</a></li>
+                    <li><a href="">Contactanos</a></li>
+                    <li><a href="">Ayuda</a></li>
+                    <li><a href=""><i class="fa-solid fa-cart-shopping fa-2xl"></i></a></li>
+                </ul>
+            </div>
         </div>
     </header>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="icon" href="img/favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/bfdec4dace.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <header>
+        <?php
+            include 'header.php';
+        ?>
+    </header>
 </body>
 </html>
