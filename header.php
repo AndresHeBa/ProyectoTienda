3eacfde5de0699d6560076bf95a9d0ec8a7ac760
AndresHeBa <124836762+AndresHeBa@users.noreply.github.com>
2023-11-30 21:34:08 -0600

    navbar

--- header.php
+++ header.php
@@ -9,22 +9,52 @@
 </head>
 
 <body>
-<header>
-        <div class="row menu">
-            <div class="col-6 head logo_head">
+    <!-- <header>
+        <div class="row menu z-3">
+            <div class="col-4 head logo_head">
                 <img id="logo" src="img/logo_transparent.png" alt="" height="60px">
             </div>
             <div class="col head">
                 <ul id="enlaces">
                     <li><a href="principal.php">Inicio</a></li>
+                    <li><a href="">Tienda</a></li>
                     <li><a href="">Sobre Nosotros</a></li>
                     <li><a href="">Contactanos</a></li>
                     <li><a href="">Ayuda</a></li>
                     <li><a href=""><i class="fa-solid fa-cart-shopping fa-2xl"></i></a></li>
+                    <li><a href="">Iniciar Sesion</a></li>
                 </ul>
             </div>
         </div>
-    </header>
+    </header> -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img id="logo" src="img/logo_transparent.png" alt="" height="50px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Tienda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Sobre Nosotros</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contactanos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Ayuda</a>
+                </li>
+            </ul>
+            <a href=""><i class="fa-solid fa-cart-shopping fa-2xl icono"></i></a>
+            </div>
+        </div>
+    </nav>
 </body>
 
 </html>
