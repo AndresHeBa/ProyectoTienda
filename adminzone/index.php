e4d2ee5a7b871518ff81018942837a930eda99c4
YuunoDev <adrianalonso.a4@proton.me>
2023-12-02 02:58:35 -0600

    Correciones

--- adminzone/index.php
+++ adminzone/index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/transition-style">
     <title>Tecnogadget - Panel de Administración</title>
 </head>
 <body>
 
 <?php include 'includes/header.php'; ?>
 
-<div class="container">
+<div class="container" transition-style="out:square:center">
     <h2>Bienvenido a Tecnogadget - Panel de Administración</h2>
     <ul>
         <li><a href="admin/users.php">Administración de Usuarios</a></li>
